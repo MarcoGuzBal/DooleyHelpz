636b821d99bfd500960456a1fc9b77daf3743b4c
MarcoGuzman-Balcazar <maguzm2@emory.edu>
2025-09-26 19:52:30 -0400

    Setup Vite + React (TS) + Tailwind CSS

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Hello, we are DooleyHelpz!</h1>
+    </div>
+  )
+}
+
+export default App
