114d333fba19bb69f91353bf4f92cd085619476b
MarcoGuzman-Balcazar <maguzm2@emory.edu>
2025-10-12 21:21:08 -0400

    Created a protected route, added DashboardPage that welcomes the users email, need to fix the user session.

---
+++ Frontend/src/pages/DashboardPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { auth } from "../firebase";
+
+function DashboardPage() {
+
+  const user = auth.currentUser;
+  return (
+    <div className="p-6">
+      {user ? (
+        <h1 className="text-xl font-semibold">
+          Hello, {user.email || "there"}!
+        </h1>
+      ) : (
+        <h1 className="text-xl font-semibold">Welcome, guest!</h1>
+      )}
+    </div>
+  )
+}
+
+export default DashboardPage
