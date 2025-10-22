e46ba6793791a26debee985b03c44bbc3210c55c
MarcoGuzman-Balcazar <maguzm2@emory.edu>
2025-10-22 19:03:45 -0400

    Working on browserSession stuff

--- frontEnd/src/firebase.ts
+++ frontEnd/src/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, setPersistence, browserSessionPersistence } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +19,8 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Keep the user signed in onl for this tab/window (Even when refreshed)
+setPersistence(auth, browserSessionPersistence).catch((error) => {
+  console.error("Error setting persistence:", error);
+});
